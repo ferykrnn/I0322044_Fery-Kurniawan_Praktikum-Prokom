6c551b20a6be892fdfe0cd606df4ddf65b8a0d54
ferykrnn <ferykurniawan@student.uns.ac.id>
2023-03-14 09:52:47 +0700

    menambahkan exercise-1.py

---
+++ exercise-1.py
@@ -0,0 +1,5 @@
+print("Fery Kurniawan") #nama
+print("I0322044") #nim
+print("Kudus") #asal
+print("Di sekitar tempat tinggal saya terdapat pasar") #sekitar tempat tinggal
+print("Harapan saya setelah lulus dari Teknik Industri adalah bisa ikut berkontribusi dalam kemajuan industri di Indonesia") #harapan lulusan ti
